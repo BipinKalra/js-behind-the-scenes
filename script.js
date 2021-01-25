324667e609da6540af294ec268d5f6a162f8a0dc
BipinKalra <bipinkalra@gmail.com>
2021-01-25 19:18:01 +0530

    playing with scopes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+function calcAge(birthYear) {
+  const age = 2021 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = "Bipin";
+calcAge(1997);
